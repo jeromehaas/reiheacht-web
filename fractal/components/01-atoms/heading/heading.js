9f004598487ce1d617bfe5faba330fc9e00d2396
Jerome Haas <hello@jeromehaas.dev>
2022-03-23 15:04:15 +0100

    Setup: teaser component

--- fractal/components/01-atoms/heading/heading.js
+++ fractal/components/01-atoms/heading/heading.js
@@ -9,9 +9,11 @@ class Heading {
 	init() {
 		if (!document.querySelector(`.js-${this.name}`)) return;
 		this.headings.forEach((heading) => {
-			const highlight = JSON.parse(heading.getAttribute('data-marked'));
+			const highlight = JSON.parse(heading.getAttribute('data-highlight'));
 			let text = heading.innerText;
-			highlight.forEach((word) => text = text.replace(word, `<span class="heading--highlight">${word}</span>`));
+			highlight && highlight.forEach((word) => {
+				text = text.replace(word, `<span class="heading--highlight">${word}</span>`);
+			}); 
 			heading.innerHTML = text;
 		})
 
