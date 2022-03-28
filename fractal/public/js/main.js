32d25aa156b71cfe3cdc4274a08191206b69ca08
Jerome Haas <hello@jeromehaas.dev>
2022-03-28 22:58:18 +0200

    Setup: cookie-banner component

--- fractal/public/js/main.js
+++ fractal/public/js/main.js
@@ -2,8 +2,10 @@ import Heading from '../../components/01-atoms/heading/heading';
 import NavigationDesktop from '../../components/03-organisms/navigation-desktop/navigation-desktop';
 import NavigationMobile from '../../components/03-organisms/navigation-mobile/navigation-mobile';
 import ReferenceSlider from '../../components/03-organisms/reference-slider/reference-slider'
+import CookieBanner from '../../components/03-organisms/cookie-banner/cookie-banner';
 
 const heading = new Heading();
 const navigationDesktop = new NavigationDesktop();
 const navigationMobile = new NavigationMobile();
 const referenceSlider = new ReferenceSlider();
+const cookieBanner = new CookieBanner();
