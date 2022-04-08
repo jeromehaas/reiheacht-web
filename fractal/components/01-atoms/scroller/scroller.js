9c8f2ee2b1e7159909072cf2fa989b78a14bf1a1
Jerome Haas <hello@jeromehaas.dev>
2022-04-08 18:44:30 +0200

    Refactor: add scroll trigger to scroller componentn in hero

---
+++ fractal/components/01-atoms/scroller/scroller.js
@@ -0,0 +1,30 @@
+import { gsap } from 'gsap';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+class Scroller {
+
+	constructor() {
+		this.name = 'scroller';
+		this.elements = {
+			arrow: document.querySelector('.scroller__icon')
+		};
+		this.init();
+	}
+
+	init() {
+		if (!document.querySelector(`.js-${this.name}`)) return;
+		console.log('scroller');
+		this.addEventListener();
+	}
+
+	addEventListener() {console.log('ko');
+		this.elements.arrow.addEventListener('click', this.scrollDown)
+	}
+
+	scrollDown() {
+		gsap.to(window, { scrollTo: '#leistungen', ease: 'power2', duration: 0.5 })
+	}
+
+}
+
+export default Scroller;
