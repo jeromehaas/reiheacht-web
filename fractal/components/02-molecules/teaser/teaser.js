45d7a851118d585cfede24882aaf14327cac775a
Jerome Haas <j.haas@cyberplanet.ch>
2022-04-12 04:22:39 +0200

    Setup: fade-in animation for teasers

---
+++ fractal/components/02-molecules/teaser/teaser.js
@@ -0,0 +1,44 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+class Teaser {
+
+  constructor() {
+    this.name = "teaser";
+    this.elements = {
+      teaser: document.querySelectorAll('.teaser')
+    }
+    this.init();
+  }
+
+  init() {
+    console.log(this.elements.teaser);
+    if (!document.querySelector(`.js-${this.name}`)) return;
+    this.setupScrollTrigger();
+  }
+
+  setupScrollTrigger() {
+    const teasers = gsap.utils.toArray(this.elements.teaser);
+    teasers.forEach((teaser) => {
+      gsap.to(teaser, {
+        scrollTrigger: {
+          trigger: teaser,
+          markers: true,
+          once: true,
+          start: "bottom 100%",
+          end: "top 100%",
+          onEnter: () => { 
+            gsap.fromTo(teaser, 
+              { y: -40, opacity: 0 },
+              { y: 0, opacity: 1, duration: 2, delay: 0.1, ease: "ease" });
+          }
+        }
+      });
+    })
+    console.log(teasers);
+  }
+
+}
+
+export default Teaser;
